17e9e403151253c57f39d68475c2665d44995652
wanyang <582822832@qq.com>
2018-05-27 14:02:38 +0800

    study

---
+++ com/wy/study/TestFile.py
@@ -0,0 +1,17 @@
+'''
+Created on 2018年5月27日
+@author: wanyang
+
+    文件的读写:file=open(filename,mode):mode表示读写模式字符串
+    r:只读,r+读写,w覆盖写,w+读写(有问题,不要用),a:追加写入,a+读写,b打开二进制文件,U支持所有换行符
+    以w的形式打开,若文件不存在,则会自动创建
+    写入中文会乱码,怎么搞网上找
+    FIXME
+'''
+write1=open("testFile.txt","w");
+write1.write("gfdgfdgf");
+write1.close();
+read1 = open("testFile.txt", 'r');
+print(read1.read());
+read1.readline();
+read1.close();
---
+++ com/wy/study/TestOS.py
@@ -0,0 +1,9 @@
+'''
+Created on 2018年5月27日
+@author: wanyang
+    系统操作
+    os.walk('path'):遍历该路径下的所有文件,返回一个元组,之后用next获得里面的值
+'''
+import os
+
+os.mkdir("/test/test/");
