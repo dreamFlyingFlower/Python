246b02da9ae6db4fec92c657bde0f08412cd33e8
wanyang <582822832@qq.com>
2018-06-04 23:33:36 +0800

    study

--- com/wy/study/TestFile.py
+++ com/wy/study/TestFile.py
@@ -6,6 +6,7 @@ Created on 2018年5月27日
     r:只读,r+读写,w覆盖写,w+读写(有问题,不要用),a:追加写入,a+读写,b打开二进制文件,U支持所有换行符
     以w的形式打开,若文件不存在,则会自动创建
     写入中文会乱码,怎么搞网上找
+    使用with也可以打开文件,并且不用关心忘记关掉文件流
     FIXME
 '''
 write1=open("testFile.txt","w");
@@ -15,3 +16,6 @@ read1 = open("testFile.txt", 'r');
 print(read1.read());
 read1.readline();
 read1.close();
+
+with open("testFile.txt", 'r') as f:
+    print("do something")
