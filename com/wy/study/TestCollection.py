8b644e54e5ad0f52f3fdc10b4e9c23585551e572
wanyang <582822832@qq.com>
2018-06-07 09:05:56 +0800

    collection

---
+++ com/wy/study/TestCollection.py
@@ -0,0 +1,12 @@
+'''
+Created on 2018年6月7日
+@author: wanyang
+
+    collections模块,内置对一些基础数据的操作
+    colle
+'''
+import collections
+
+defaultMap = collections.defaultdict(list);
+defaultMap["le"]
+print(defaultMap)
